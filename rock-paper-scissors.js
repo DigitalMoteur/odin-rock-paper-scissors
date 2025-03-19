b6cc5bd69b06c7b88f7feab0d6ccc69e095b62ce
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-18 21:16:42 -0700

    Added computer choice (Rock/Paper/Scissors)

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+function getComputerChoice() {
+    switch (Math.floor(3 * Math.random())) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
