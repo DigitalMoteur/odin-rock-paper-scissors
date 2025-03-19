ce09fc8cde0be0061c7e18f2efa5cd3b7b6b7cbe
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-18 21:25:58 -0700

    Broke choice logic into its own function. Added human choice.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,5 @@
-function getComputerChoice() {
-    switch (Math.floor(3 * Math.random())) {
+function getChoice(selection) {
+    switch (selection) {
         case 0:
             return "Rock";
         case 1:
@@ -8,3 +8,20 @@ function getComputerChoice() {
             return "Scissors";
     }
 }
+
+function getComputerChoice() {
+    return getChoice(Math.floor(3 * Math.random()));
+}
+
+function getHumanChoice() {
+    const promptMessage =
+    "Select your choice!" +
+    "\n 0: Rock" +
+    "\n 1: Paper" +
+    "\n 2: Scissors";
+    const humanSelection = parseInt(prompt(promptMessage));
+    // Not great error checking, but just clamp to 0, 1, 2 and get a valid choice.
+    return getChoice(Math.min(Math.max(humanSelection, 0), 2));
+}
+
+console.log(getHumanChoice());
